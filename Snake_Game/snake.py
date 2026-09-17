329edd8583532a741d09a472d03b00047e8b5e9d
apple <apple@MacBook-Pro.local>
2026-09-18 01:31:39 +0400

    Add Snake Game

---
+++ Snake_Game/snake.py
@@ -0,0 +1,45 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 20
+RIGHT = 0
+UP = 90
+LEFT = 180
+DOWN = 270
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for i in range(3):
+            square = Turtle("square")
+            square.color("white")
+            square.penup()
+            square.goto(x=0 + (i * -20), y=0)
+            self.segments.append(square)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
